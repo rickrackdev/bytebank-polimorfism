e806d23a5f2277d85156f9910f47da742e033325
rick <alvarenga409@gmail.com>
2023-08-30 22:30:12 -0600

    created the first object class for the bank workers, setting up constructor, getters and setter; also created a file to do tests

---
+++ src/Workers.java
@@ -0,0 +1,37 @@
+public class Workers {
+
+    //creating the attributes of the workers object
+    private String name;
+    private String document;
+    private double salary;
+
+    //creating our constructor method
+    public Workers(){
+
+    }
+
+    //setting up getters and setters for each attribute
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
